5203a397e743ef82ec22cae96cbb0eea6beba3a8
minhta <minhta@vnpt.vn>
2023-06-30 17:28:32 +0700

    Add source code i18n, centent nagotiation support application/xml

--- src/main/java/com/minhta/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/minhta/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.minhta.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping( path = "/hello-world")
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
@@ -14,5 +24,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldI18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("hello.world.message", null, "Default message", locale);
     }
 }
